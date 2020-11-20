22c43274c6ca6b243718d116ef91e5641588f4fc
unknown <trongthu38@gmail.com>
2020-11-20 23:47:05 +0700

    add

--- Data/CNN_Training/src_thay_Nghia/Sgolay.m
+++ Data/CNN_Training/src_thay_Nghia/Sgolay.m
@@ -2,10 +2,11 @@
 clear ; clc; close all;
 load('Dx.mat');
 Dx=Dx';
-for i = 1:20
-    Dx_Sgolay(:,i) = sgolayfilt(Dx(:,i),97,99);
+%for i = 1:20
+i = 1;
+    Dx_Sgolay(:,i) = sgolayfilt(Dx(:,i),5,7);
     figure
     plot(Dx(:,i)); hold on;
     plot(Dx_Sgolay(:,i));
-end
+%end
 Dx_Sgolay = Dx_Sgolay';
